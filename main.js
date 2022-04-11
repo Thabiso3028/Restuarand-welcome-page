7fd6a38dba7a684e7eaf235412ae53b9b94f3970
Thabiso Kubeka <65237633+Thabiso3028@users.noreply.github.com>
2022-04-12 01:07:11 +0200

    Services section

--- main.js
+++ main.js
@@ -3,17 +3,4 @@ window.addEventListener('scroll', function(){
     header.classList.toggle("sticky", window.scrollY > 0);
   });
 
-  (function (){
-
-    'use strict';
   
-    $('ul.navigation >li').on('click', function(e){
-  
-      e.preventDefualt();
-  
-      $('ul.navigation >li').removeClass('active');
-      $(this).addClass('active');
-  
-  
-    });
-  });(jQuery)
