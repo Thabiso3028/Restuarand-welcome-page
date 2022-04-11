51d7f0085da3f50624ec990c2b342ce31163e7e5
Thabiso Kubeka <65237633+Thabiso3028@users.noreply.github.com>
2022-04-11 13:25:37 +0200

    colors

--- main.js
+++ main.js
@@ -2,3 +2,18 @@ window.addEventListener('scroll', function(){
     const header = document.getElementById('header');
     header.classList.toggle("sticky", window.scrollY > 0);
   });
+
+  (function (){
+
+    'use strict';
+  
+    $('ul.navigation >li').on('click', function(e){
+  
+      e.preventDefualt();
+  
+      $('ul.navigation >li').removeClass('active');
+      $(this).addClass('active');
+  
+  
+    });
+  });(jQuery)
